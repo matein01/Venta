c689d994383aff4d95c3ef7cb9f11cb80361270a
Mateo Guerrero Ordoñez <mateo.guerrero.or@gmail.com>
2022-07-28 12:12:08 -0500

    Lista de productos agregada

--- assets/code.js
+++ assets/code.js
@@ -4,6 +4,7 @@ const hamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const carritoIcon = document.querySelector(".navbar-shopping-cart");
 const productDetail = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
 
 navEmail.addEventListener("click", toggleDesktopMenu);
 hamIcon.addEventListener("click", toggleMobileMenu);
@@ -43,3 +44,106 @@ function toggleProductDetail() {
 
     productDetail.classList.toggle("inactive");
 }
+
+const productList = [];
+
+productList.push({
+    name: "Motorcycle",
+    price: 520,
+    image: "assets/img/Motorcycle.jpg"
+});
+productList.push({
+    name: "Bike",
+    price: 120,
+    image: "assets/img/Bike.jpg"
+});
+productList.push({
+    name: "Computer",
+    price: 620,
+    image: "assets/img/Computer.jpg"
+});
+productList.push({
+    name: "Motorcycle",
+    price: 520,
+    image: "assets/img/Motorcycle.jpg"
+});
+productList.push({
+    name: "Bike",
+    price: 120,
+    image: "assets/img/Bike.jpg"
+});
+productList.push({
+    name: "Computer",
+    price: 620,
+    image: "assets/img/Computer.jpg"
+});
+productList.push({
+    name: "Motorcycle",
+    price: 520,
+    image: "assets/img/Motorcycle.jpg"
+});
+productList.push({
+    name: "Bike",
+    price: 120,
+    image: "assets/img/Bike.jpg"
+});
+productList.push({
+    name: "Computer",
+    price: 620,
+    image: "assets/img/Computer.jpg"
+});
+productList.push({
+    name: "Motorcycle",
+    price: 520,
+    image: "assets/img/Motorcycle.jpg"
+});
+productList.push({
+    name: "Bike",
+    price: 120,
+    image: "assets/img/Bike.jpg"
+});
+productList.push({
+    name: "Computer",
+    price: 620,
+    image: "assets/img/Computer.jpg"
+});
+
+function viewProducts(array) {
+    for(product of array) {
+        const productCard = document.createElement("div");
+        productCard.classList.add("product-card");
+    
+        const productImg = document.createElement("img");
+        productImg.setAttribute("src", product.image);
+    
+        const productInfo = document.createElement("div");
+        productInfo.classList.add("product-info");
+    
+        const divInfo = document.createElement("div");
+    
+        const productPrice = document.createElement("p");
+        productPrice.innerText = "$" + product.price;
+    
+        const productName = document.createElement("p");
+        productName.innerText = product.name;
+    
+        divInfo.appendChild(productPrice);
+        divInfo.appendChild(productName);
+    
+        const productFigure = document.createElement("figure");
+        const cartImg = document.createElement("img");
+        cartImg.setAttribute("src", "./assets/icons/bt_add_to_cart.svg");
+    
+        productFigure.appendChild(cartImg);
+    
+        productInfo.appendChild(divInfo);
+        productInfo.appendChild(productFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+viewProducts(productList);
